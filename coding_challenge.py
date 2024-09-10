8c20d8c54b7a86a3377365c4e9239bf395e3869e
Vishnu Ajith <vishnu.ajith1010@gmail.com>
2024-09-10 18:17:55 +0530

    refactor: created improved version of the previous function

--- coding_challenge.py
+++ coding_challenge.py
@@ -1,3 +1,8 @@
+from collections.abc import Iterable as IterableClass
+from collections import Counter
+from typing import Any, Dict, Iterable
+
+
 def f(s):
     """
     This function counts the frequency of the elements present in the iterable `s`.
@@ -16,6 +21,22 @@ def f(s):
             r[i] = 1
     return r
 
+def count_frequency(s: Iterable[Any]) -> Dict[Any, int]:
+    """
+    This function counts the frequency of the elements present in the iterable `s`.
+
+    Args:
+        s (iterable): This is an iterable that is provided.
+
+    Returns:
+        dict: A dict containing the elements of `s` and the frequencies as values.
+    """
+    if not isinstance(s, IterableClass):
+        raise TypeError("Input must be an iterable")
+
+    return Counter(s)
+
 
 if __name__ == "__main__":
-    print(f("malayalam"))
+    print("Original------>", f("malayalam"))
+    print("Modified------>", count_frequency("malayalam"))
